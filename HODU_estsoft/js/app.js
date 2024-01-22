48114298b2f99fd730c1949f6988373383810833
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-01-22 16:37:35 +0900

    feature fixed CSS for lorem picsum fixed the problem of restarting the page everytime subscribe button was pushed added js functions for popup
    there is a problem with popup that when it apears, blank space spawns pushing every component below downwards. it needs to be fixed

--- HODU_estsoft/js/app.js
+++ HODU_estsoft/js/app.js
@@ -1,10 +1,10 @@
-const imageList = document.querySelector("#image-list");
+const imageList = document.querySelector(".article-image-list");
 
 let pageToFetch = 1;
 
 async function fetchImages(pageNum){
   try {
-    const response = await fetch('https://picsum.photos/v2/list?page='+pageNum+'&limit=3');
+    const response = await fetch('https://picsum.photos/v2/list?page='+pageNum+'&limit=9');
     if (!response.ok) {
       throw new Error('네트워크 응답에 문제가 있습니다.');
     }
@@ -20,16 +20,27 @@ async function fetchImages(pageNum){
 }
 const showMoreBtn = document.querySelector(".show-more-btn")
 
-function makeImageList(datas){
-  datas.forEach((item)=>{
-    imageList.innerHTML = imageList.innerHTML + "<li><img class=`article-img` src=`"+ item.download_url +"` alt=''></li>";
-  });
+function makeImageList(datas) {
+  for (let i = 0; i < datas.length; i += 3) {
+    const ul = document.createElement("ul");
+    ul.className = "article-img-cell";
+
+    // 각 <ul>에 최대 3개의 이미지를 추가합니다.
+    for (let j = i; j < i + 3 && j < datas.length; j++) {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      img.className = "article-img";
+      img.src = datas[j].download_url;
+      img.alt = "lorem picsum";
+      li.appendChild(img);
+      ul.appendChild(li);
+    }
+
+    imageList.appendChild(ul);
+  }
 }
 
 showMoreBtn.addEventListener('click',()=>{
   fetchImages(pageToFetch++)
 });
 
-
-
-fetchImages();
--- HODU_estsoft/js/subscribe-popup.js
+++ HODU_estsoft/js/subscribe-popup.js
@@ -1,2 +1,10 @@
-const scrollTopBtn= document.querySelector(".subscribe-btn")
+document.querySelector('.subscribe-btn').addEventListener('click', function() {
+  var popup = document.querySelector('.thank-you-popup');
+  popup.style.display = 'flex';
+});
+
+document.querySelector('.love-you-btn').addEventListener('click', function() {
+  var popup = document.querySelector('.thank-you-popup');
+  popup.style.display = 'none';
+});
 
